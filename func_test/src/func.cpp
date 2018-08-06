fc6662aa6fbc49deb89fe2cf9405957697d2bc3f
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-08-06 20:08:40 +0900

    Replace func func-dir

---
+++ func_test/src/func.cpp
@@ -0,0 +1,6 @@
+#include "func.hpp"
+
+int func(int a, int b)
+{
+  return a * b * b * b;
+}
---
+++ func_test/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "func.hpp"
+
+int main()
+{
+  std::cout << func(1, 2) << '\n';
+}
