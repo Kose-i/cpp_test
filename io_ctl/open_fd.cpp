24abe0a6c15f7f86525cf93a4d046a2feae7c013
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-10 18:21:29 +0900

    Add .gitignore

--- io_ctl/open_fd.cpp
+++ io_ctl/open_fd.cpp
@@ -8,6 +8,8 @@ int main(int argc, char** argv)
 {
   int first_fd = open("first_fd",O_RDONLY | O_EXCL | O_CREAT);
   int second_fd = open("second_fd",O_RDONLY | O_EXCL | O_CREAT);
+  std::cout << "first_fd:" << first_fd << '\n';
+  std::cout << "second_fd:" << second_fd << '\n';
   close(first_fd);
   close(second_fd);
 }
