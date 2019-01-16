dd933268140498bc4a8818fae77626194b03773b
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-16 15:15:47 +0900

    Change constructor test

--- class_test/main.cpp
+++ class_test/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 class A{
+  public:
+    A(){}
+    A(const int&)
+    {
+      std::cout << "A constructor A(int)\n";
+    }
   protected:
     void protect_test(){
       std::cout << "A class protect_test()\n";
@@ -14,6 +20,10 @@ class A{
 class B: public A
 {
   public:
+
+    B(){}
+    B(const int& a):A(a){}
+
     void test(){
       std::cout << "B class test()\n";
     }
@@ -25,4 +35,5 @@ class B: public A
 int main(int argc, char** argv){
   B a;
   a.test();
+  B b(3);
 }
